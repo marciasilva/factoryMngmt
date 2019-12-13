9971fed63f519b72f9cfe2f21a9e7fc84bc337b2
Marcia Nasare Silva <marcia.nasare.silva@ericsson.com>
2019-12-12 22:41:17 -0300

    Create multi project structure using maven

---
+++ factory-model/src/main/java/com/factory/model/defaultValues/IRootEntity.java
@@ -0,0 +1,5 @@
+package com.factory.model.defaultValues;
+
+public interface IRootEntity {
+
+}
---
+++ factory-model/src/main/java/com/factory/model/entities/Order.java
@@ -0,0 +1,7 @@
+package com.factory.model.entities;
+
+import com.factory.model.defaultValues.IRootEntity;
+
+public class Order implements IRootEntity{
+
+}
